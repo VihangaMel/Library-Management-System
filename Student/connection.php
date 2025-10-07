78b1acb2a4898c246fe4630c946b833ed4e3d3d1
Vihanga Melindra <vihangamelindra@gmail.com>
2025-10-07 13:29:28 +0530

    creating registration function

---
+++ Student/connection.php
@@ -0,0 +1,8 @@
+<?php
+    $db = mysqli_connect("localhost","root","","lms");
+
+    // if(!$db){
+    //     die("Connection failed: " . mysqli_connect_error());
+    // }
+    // echo "Connected successfully";
+?>
--- Student/registration.php
+++ Student/registration.php
@@ -1,128 +1,72 @@
 <?php
+include "connection.php";
 include "navbar.php";
+
+// Handle form submission
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
+    $fname = $_POST['firstname'];
+    $lname = $_POST['lastname'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $roll = $_POST['roll'];
+    $email = $_POST['email'];
+
+    // Check if username already exists
+    $stmt = $db->prepare("SELECT COUNT(*) FROM Student WHERE username = ?");
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    $stmt->bind_result($count);
+    $stmt->fetch();
+    $stmt->close();
+
+    if ($count == 0) {
+        // Insert new user
+        $stmt = $db->prepare("INSERT INTO Student (firstname, lastname, username, password, roll, email) VALUES (?, ?, ?, ?, ?, ?)");
+        $stmt->bind_param("ssssss", $fname, $lname, $username, $password, $roll, $email);
+        $stmt->execute();
+        $stmt->close();
+
+        echo "<script>alert('Registration successful.');</script>";
+    } else {
+        echo "<script>alert('The username already exists.');</script>";
+    }
+}
 ?>
 
 <!DOCTYPE html>
 <html>
-  <head>
+<head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Student Registration</title>
-    <link rel="stylesheet" type="text/css" href="style.css" />
-    <!-- Font Awesome CDN -->
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
-    />
-  </head>
-  <body>
+    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+</head>
+<body>
     <div class="wrapper">
-      <!-- <header>
-        <div class="logo-container">
-          <div class="logo">
-            <img src="Images/logo3.png" />
-          </div>
-          <h1>ONLINE LIBRARY MANAGEMENT SYSTEM</h1>
-        </div>
-
-        <nav>
-          <ul>
-            <li><a href="index.php">HOME</a></li>
-            <li><a href="books.php">BOOKS</a></li>
-            <li><a href="feedback.php">FEEDBACK</a></li>
-          </ul>
-        </nav>
-
-        <div class="user-actions">
-          <a href="student_login.php">
-            <i class="fas fa-sign-in-alt"></i>
-            LOGIN
-          </a>
-          <a href="">
-            <i class="fas fa-sign-out-alt"></i>
-            LOGOUT
-          </a>
-          <a href="registration.php">
-            <i class="fas fa-user-plus"></i>
-            SIGN UP
-          </a>
-        </div>
-      </header> -->
-      <section>
-        <div class="reg_img">
-          <br />
-          <div class="box2">
-            <h1
-              style="
-                text-align: center;
-                font-size: 35px;
-                font-family: serif;
-                color: white;
-              "
-            >
-              Library Management System
-            </h1>
-            <br />
-            <h1 style="text-align: center; font-size: 25px; color: white">
-              User Registration Form
-            </h1>
-            <br />
-            <form name="Registration" action="" method="">
-              <div class="Login">
-                <input
-                  class="form-control"
-                  type="text"
-                  name="firstname"
-                  placeholder="First Name"
-                  required=""
-                /><br /><br />
-                <input
-                  class="form-control"
-                  type="text"
-                  name="lastname"
-                  placeholder="Last Name"
-                  required=""
-                /><br /><br />
-                <input
-                  class="form-control"
-                  type="text"
-                  name="username"
-                  placeholder="Username"
-                  required=""
-                /><br /><br />
-                <input
-                  class="form-control"
-                  type="password"
-                  name="password"
-                  placeholder="Password"
-                  required=""
-                /><br /><br />
-                <input
-                  class="form-control"
-                  type="text"
-                  name="roll"
-                  placeholder="Roll No"
-                  required=""
-                /><br /><br />
-                <input
-                  class="form-control"
-                  type="text"
-                  name="email"
-                  placeholder="Email"
-                  required=""
-                /><br /><br />
-                <input
-                  class="btn btn-default"
-                  type="submit"
-                  name="submit"
-                  value="Sign Up"
-                  style="color: black; width: 70px; height: 30px"
-                />
-              </div>
-            </form>
-          </div>
-        </div>
-      </section>
+        <section>
+            <div class="reg_img">
+                <div class="box2">
+                    <h1 style="text-align: center; font-size: 35px; font-family: serif; color: white;">
+                        Library Management System
+                    </h1>
+                    <h2 style="text-align: center; font-size: 25px; color: white;">
+                        User Registration Form
+                    </h2>
+                    <form method="POST" action="">
+                        <div class="Login">
+                            <input class="form-control" type="text" name="firstname" placeholder="First Name" required><br><br>
+                            <input class="form-control" type="text" name="lastname" placeholder="Last Name" required><br><br>
+                            <input class="form-control" type="text" name="username" placeholder="Username" required><br><br>
+                            <input class="form-control" type="password" name="password" placeholder="Password" required><br><br>
+                            <input class="form-control" type="text" name="roll" placeholder="Roll No" required><br><br>
+                            <input class="form-control" type="email" name="email" placeholder="Email" required><br><br>
+                            <input class="btn btn-default" type="submit" name="submit" value="Sign Up" style="color: black; width: 70px; height: 30px;">
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </section>
     </div>
-  </body>
+</body>
 </html>
